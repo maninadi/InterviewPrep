9ad8ac5bc0376cfa1a20ce74b3ac5509a15aa345
Manikandan Nadimuthu <njmanikandan@gmail.com>
2017-02-03 01:49:18 -0600

    Added zigzagstring to strings

---
+++ Strings/ZigZagString.java
@@ -0,0 +1,63 @@
+package Strings;
+
+/*
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+	P.......A........H.......N
+	..A..P....L....S....I...I....G
+	....Y.........I........R
+	And then read line by line: PAHNAPLSIIGYIR
+	Write the code that will take a string and make this conversion given a number of rows:
+	
+	string convert(string text, int nRows);
+	convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR"
+	**Example 2 : **
+	ABCD, 2 can be written as
+	
+	A....C
+	...B....D
+	and hence the answer would be ACBD.
+ */
+
+
+public class ZigZagString {
+
+	public static void main(String[] args){
+		
+		System.out.println(zigzag("PAYPALISHIRING",3));
+		System.out.println(zigzag("ABCD",2));
+	}
+	
+	static String zigzag(String s,int rows){
+		
+		String[] strs=new String[rows];
+		
+		int i=0;
+		int add=1;
+		int cnt=0;
+		boolean flip=false;
+		while(cnt<s.length()){
+			
+			if(strs[i]==null)
+				strs[i]=new String();
+			strs[i]+=s.charAt(cnt);
+			cnt++;
+			
+			if((i==rows-1)||(i==0)){
+				flip=!flip;
+				
+			}
+			add=flip?1:-1;
+			i+=add;
+		}
+		
+		StringBuilder sb=new StringBuilder();
+		
+		for(String s1:strs){
+			sb.append(s1);
+		}
+				
+		return sb.toString();
+	}
+	
+}
