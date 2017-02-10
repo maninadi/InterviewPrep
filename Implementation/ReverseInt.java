ac5a736b64c41dfcecf2d7650f3e0c816625e62a
Manikandan Nadimuthu <njmanikandan@gmail.com>
2017-02-09 22:34:37 -0600

    Added ReverseInt

---
+++ Implementation/ReverseInt.java
@@ -0,0 +1,28 @@
+package Implementation;
+
+public class ReverseInt {
+
+	public static void main(String[] args){
+		
+		System.out.println(reverse(123));
+		System.out.println(reverse(90909777));
+		System.out.println(reverse(Integer.MAX_VALUE));
+		System.out.println(reverse(Integer.MIN_VALUE));
+	}
+	
+	static int reverse(int a) {
+	    
+	    long res=0;
+	    long multi=a<0?-1:1;
+	    a=Math.abs(a);
+	    while(a>0){
+	        res*=10;
+	        res+=(a%10);
+	        a/=10;
+	    }
+	    if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE)
+	        res=0;
+	    
+	    return (int)(res*multi);
+	}
+}
